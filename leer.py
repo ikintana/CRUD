e5ae9a4bcc3bfaf69a24ed6333328a0c884e769e
benatmartineztx <145110760+benatmartineztx@users.noreply.github.com>
2023-09-29 09:12:59 +0200

    leer hecho

--- leer.py
+++ leer.py
@@ -1,4 +1,5 @@
 import listado as l
 
 def mostrar_vuelos():
-    pass
+    for diccionario in l.lista:
+        print(f"Fecha de Salida: {diccionario['fecha_completa']}, ID Vuelo:  {diccionario['id_vuelos']}), Destino: {diccionario['destino']}, Plazas Libres: {diccionario['plazas_libres']}")
