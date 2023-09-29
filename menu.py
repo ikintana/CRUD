018114aa98d7c3066228dd3f195f790438356a7d
miguellopezhe <145114070+miguellopezhe@users.noreply.github.com>
2023-09-29 09:52:23 +0200

    menu 2.0

--- menu.py
+++ menu.py
@@ -2,9 +2,10 @@ import leer
 import aniadir
 import modificar
 import eliminar
+import listado as l
 
 def ejecutar():
-
+    l.cargar()
     variable = True
     while variable:
             print("\n- - - - - - - - - - - - - - - - - - - -")
@@ -26,7 +27,7 @@ def ejecutar():
                     print("\nOpción no válida. Intente de nuevo.")
 
 def mostrar_menu():
-    print("n\1. Información de los Vuelos")
+    print("\n1. Información de los Vuelos")
     print("2. Añadir un Vuelo")
     print("3. Modificar un Vuelo Existente")
     print("4. Borrar un Vuelo")
