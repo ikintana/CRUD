f246e68502e47f2fed30947b84ba058e8ccd1f06
benatmartineztx <145110760+benatmartineztx@users.noreply.github.com>
2023-09-29 09:28:17 +0200

    corrección ortografica

--- leer.py
+++ leer.py
@@ -2,4 +2,4 @@ import listado as l
 
 def mostrar_vuelos():
     for diccionario in l.lista:
-        print(f"Fecha de Salida: {diccionario['fecha_completa']}, ID Vuelo:  {diccionario['id_vuelos']}), Destino: {diccionario['destino']}, Plazas Libres: {diccionario['plazas_libres']}")
+        print(f"Fecha de Salida: {diccionario['fecha_completa']}, ID Vuelo:  {diccionario['id_vuelo']}), Destino: {diccionario['destino']}, Plazas Libres: {diccionario['plazas_libres']}")
